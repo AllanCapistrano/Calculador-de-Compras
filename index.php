4d980a3c5564bb2b853a6be9c58bd85625f0d710
AllanCapistrano <allan.capistrano3@gmail.com>
2020-09-05 15:20:25 -0300

    Corrigindo identação.

--- index.php
+++ index.php
@@ -9,20 +9,20 @@
 <body>
   <h1>Calculador de compras</h1>
   <div id="interface">
-      <form method="POST" action="./src/form.php" onsubmit="return false">
-          <section>
-              <h3>Fez compras para quantas pessoas?</h3>
-              <select name="qtd-pessoas" id="iqtd-pessoas" onchange="this.form.submit()">
-                  <option value="" selected></option>
-                  <?php
-                      for($i=1; $i <=10; $i++){
-                          echo "<option value='$i'>$i</option>";
-                      }
-                  ?>
-              </select>
-          </section>
-      </form>
-      <img src="./assets/shopping-cart.png" id="carro-compra">
+    <form method="POST" action="./src/form.php" onsubmit="return false">
+      <section>
+        <h3>Fez compras para quantas pessoas?</h3>
+        <select name="qtd-pessoas" id="iqtd-pessoas" onchange="this.form.submit()">
+          <option value="" selected></option>
+          <?php
+            for($i=1; $i <=10; $i++){
+              echo "<option value='$i'>$i</option>";
+            }
+          ?>
+        </select>
+      </section>
+    </form>
+    <img src="./assets/shopping-cart.png" id="carro-compra">
   </div>
 
   <script src="./scripts/functions.js"></script>
